1c61ae234483f8436511d19a979d04999fe906fb
Samy Pessé <samypesse@gmail.com>
2015-04-09 16:38:43 +0200

    Fix hr.utils dependency

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 var settings = require("./settings");
 
-var _ = codebox.require("hr/utils");
+var _ = codebox.require("hr.utils");
 var commands = codebox.require("core/commands");
 var rpc = codebox.require("core/rpc");
 var dialogs = codebox.require("utils/dialogs");
