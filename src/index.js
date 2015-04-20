f33d107e0d7b995ec4154ed4991b61f1d0c7f3dc
Samy Pesse <samypesse@gmail.com>
2015-04-20 09:41:54 -0500

    Remove useless menu

--- src/index.js
+++ src/index.js
@@ -5,18 +5,3 @@ var commands = codebox.require("core/commands");
 var rpc = codebox.require("core/rpc");
 var dialogs = codebox.require("utils/dialogs");
 
-codebox.menubar.createMenu({
-    id: "project",
-    caption: "Project",
-    items: [
-        {
-            caption: "New Sample Project",
-            command: "project.init"
-        },
-        {
-            id: "deploy",
-            caption: "Deploy to",
-            items: []
-        }
-    ]
-});
